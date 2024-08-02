5b1dc9896d11d00f0ab4cc76ac2d3e515c0db550
Olivier Reynard <olivier.reynard17@gmail.com>
2024-08-02 23:23:23 +0200

    back to school projet progress

--- projects/back-to-school/js/Boid.js
+++ projects/back-to-school/js/Boid.js
@@ -10,7 +10,25 @@ export default class Boid{
         //velocity as a vector with components in the x and y directions
         this.velocity = velocity
         this.mass = mass
-        this.radius = Math.cbrt(this.mass)
+        this.radius = 10
         this.color = "red"
+    }
+
+
+    next(){
+        const [ax, ay] = this.acceleration
+
+        let v = [this.velocity[0] + ax*this.dt , this.velocity[1] + ay*this.dt] // velocity in [x, y]
+        this.velocity = v
+
+        let r = [this.position[0] + this.velocity[0]*this.dt , this.position[1] + this.velocity[1]*this.dt] // position in [x, y]
+        this.position = r
+    }
+
+    draw(){
+        this.context.beginPath();
+        this.context.arc(this.position[0], this.position[1], this.radius, 0, 2 * Math.PI);
+        this.context.fillStyle = this.color
+        this.context.fill();
     }
 }
--- projects/back-to-school/js/Space.js
+++ projects/back-to-school/js/Space.js
@@ -5,13 +5,69 @@ import Boid from './Boid.js';
 export default class Space {
     constructor(context){
         this.context = context
-        this.bodies = []
+        this.boids = []
         this.dt = 0.1 // increase for bigger movements per refresh, decrease for smaller movements per refresh
+
+        this.flockRadius = 200 // in px, an individual boid maneuvers based on the positions and velocities its nearby flockmates, flockRadius put a value to "nearby"
+        this.steeringMaxForce = 0.5 // steering is a force to incentivize a boid to move alongside its flockmates
 
         this.initialize()
     }
 
+    next(){
+        this.boids.forEach(b => {
+            b.acceleration = this.getSteeringForce(b)
+        })
+
+        this.boids.forEach(b => {
+            b.next()
+        })
+    }
     
+    // (Boid) : [number, number]
+    // steering is a force to incentivize a boid to move alongside its flockmates, it will change the acceleration of a boid
+    // it is the difference between the current velocity and the desired velocity (an average of the flockmates' velocities)
+    getSteeringForce(boid){
+        let steeringForce = [0, 0]
+        let ct = 0
+        for(let i=0 ; i<this.boids.length ; i++){
+            // The strict equality operator checks for reference equality when it comes to objects.
+            let distance = this.getDistanceBetweenTwoBoids(boid, this.boids[i])
+            if(boid !== this.boids[i] && distance <= this.flockRadius){
+                ct++
+                steeringForce[0] += boid.velocity[0]
+                steeringForce[1] += boid.velocity[1]
+            }
+        }
+
+        // averaging
+        steeringForce[0] /= ct
+        steeringForce[1] /= ct
+
+        steeringForce[0] = boid.velocity[0] - steeringForce[0]
+        steeringForce[1] = boid.velocity[1] - steeringForce[1]
+
+        return steeringForce
+    }
+
+    // (Boid, Boid) : number
+    getDistanceBetweenTwoBoids(boid1, boid2){
+        let [px1, py1] = boid1.position
+        let [px2, py2] = boid2.position
+
+        return Math.sqrt(Math.pow(px1-px2, 2) + Math.pow(py1-py2, 2))
+    }
+
+    initialize(){
+        for(let i=0 ; i<40 ; i++){
+            this.boids.push(new Boid(this.context, this.dt, [0, 0], [this.randomNumberBetween(1, 5), this.randomNumberBetween(1, 5)], [this.randomNumberBetween(1, WIDTH), this.randomNumberBetween(1, HEIGHT)], 1))
+        }
+    }
+
+    draw(){
+        this.boids.forEach(b => b.draw())
+    }
+
 
     //=================================================================
     //==================== ALL PURPOSE FUNCTIONS ======================
